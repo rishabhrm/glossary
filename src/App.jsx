e3655792add6b1e2472fcf78f997de3dee29c9ed
Rishabh <113576736+rishabhrm@users.noreply.github.com>
2024-09-08 17:01:18 +0530

    added home page design

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,31 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      Hello World!
-    </>
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1 className="heading">Glossary</h1>
+        </header>
+        <main>
+          components
+        </main>
+        <footer className="mt-5 footer">
+          <div className="footer-content">
+            <p className="credit">
+              Coded by{" "}
+              <a
+                href="https://github.com/rishabhrm"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-link"
+              >
+                rishabhrm.
+              </a>
+            </p>
+          </div>
+        </footer>
+      </div>
+    </div>
   )
 }
 
